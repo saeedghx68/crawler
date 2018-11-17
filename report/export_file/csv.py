12b2e88c19d85a14f59b90bdab019d381e92475d
Saeed Gharedaghi <saeed.ghx68@gmail.com>
2018-11-17 10:57:35 +0330

    prevent to give excetions

--- report/export_file/csv.py
+++ report/export_file/csv.py
@@ -4,6 +4,12 @@ import csv
 class CSV:
     @classmethod
     def export_file(cls, data, file_address):
+        """
+        export csv file in file_address path
+        :param data: the data that we want to create csv file
+        :param file_address: path of file address
+        :return:
+        """
         with open(file_address, mode='w') as csv_file:
             fieldnames = [
                 'url',
@@ -18,8 +24,8 @@ class CSV:
                 writer.writerow({
                     'url': url,
                     'urls': ','.join(items['urls']),
-                    'css_links': ','.join(items['css_links']),
-                    'js_links': ','.join(items['js_links']),
-                    'img_links': ','.join(items['img_links']),
-                    'icon_links': ','.join(items['icon_links']),
+                    'css_links': ','.join(items.get('css_links', [])),
+                    'js_links': ','.join(items.get('js_links', [])),
+                    'img_links': ','.join(items.get('img_links', [])),
+                    'icon_links': ','.join(items.get('icon_links', [])),
                 })
--- report/export_file/xml.py
+++ report/export_file/xml.py
@@ -4,54 +4,64 @@ from xml.dom import minidom
 class XML:
     @classmethod
     def export_file(cls, data, file_address):
+        """
+        export xml file in file_address path
+        :param data: the data that we want to create csv file
+        :param file_address: path of file address
+        :return:
+        """
+
+        # create root element
         root = minidom.Document()
         xml = root.createElement('root')
         root.appendChild(xml)
+
+        # loop over data to create xml node
         for url, items in data:
-            urlChild = root.createElement("url")
-            urlChild.setAttribute('link', url)
+            url_child = root.createElement("url")
+            url_child.setAttribute('link', url)
 
             # add url
-            childOfUrl = root.createElement('urls')
-            urlChild.appendChild(childOfUrl)
-            for link in items['urls']:
-                childOfChildOfUrl = root.createElement('url')
-                childOfChildOfUrl.appendChild(root.createTextNode(link))
-                childOfUrl.appendChild(childOfChildOfUrl)
+            child_of_url = root.createElement('urls')
+            url_child.appendChild(child_of_url)
+            for link in items('urls', []):
+                child_of_child_of_url = root.createElement('url')
+                child_of_child_of_url.appendChild(root.createTextNode(link))
+                child_of_url.appendChild(child_of_child_of_url)
 
             # add css links
-            childOfUrl = root.createElement('css_links')
-            urlChild.appendChild(childOfUrl)
-            for link in items['css_links']:
-                childOfChildOfUrl = root.createElement('css_link')
-                childOfChildOfUrl.appendChild(root.createTextNode(link))
-                childOfUrl.appendChild(childOfChildOfUrl)
+            child_of_url = root.createElement('css_links')
+            url_child.appendChild(child_of_url)
+            for link in items.get('css_links', []):
+                child_of_child_of_url = root.createElement('css_link')
+                child_of_child_of_url.appendChild(root.createTextNode(link))
+                child_of_url.appendChild(child_of_child_of_url)
 
             # add js links
-            childOfUrl = root.createElement('js_links')
-            urlChild.appendChild(childOfUrl)
-            for link in items['js_links']:
-                childOfChildOfUrl = root.createElement('js_link')
-                childOfChildOfUrl.appendChild(root.createTextNode(link))
-                childOfUrl.appendChild(childOfChildOfUrl)
+            child_of_url = root.createElement('js_links')
+            url_child.appendChild(child_of_url)
+            for link in items.get('js_links', []):
+                child_of_child_of_url = root.createElement('js_link')
+                child_of_child_of_url.appendChild(root.createTextNode(link))
+                child_of_url.appendChild(child_of_child_of_url)
 
             # add img links
-            childOfUrl = root.createElement('img_links')
-            urlChild.appendChild(childOfUrl)
-            for link in items['img_links']:
-                childOfChildOfUrl = root.createElement('img_link')
-                childOfChildOfUrl.appendChild(root.createTextNode(link))
-                childOfUrl.appendChild(childOfChildOfUrl)
+            child_of_url = root.createElement('img_links')
+            url_child.appendChild(child_of_url)
+            for link in items.get('img_links', []):
+                child_of_child_of_url = root.createElement('img_link')
+                child_of_child_of_url.appendChild(root.createTextNode(link))
+                child_of_url.appendChild(child_of_child_of_url)
 
             # add icon links
-            childOfUrl = root.createElement('icon_links')
-            urlChild.appendChild(childOfUrl)
-            for link in items['icon_links']:
-                childOfChildOfUrl = root.createElement('icon_link')
-                childOfChildOfUrl.appendChild(root.createTextNode(link))
-                childOfUrl.appendChild(childOfChildOfUrl)
+            child_of_url = root.createElement('icon_links')
+            url_child.appendChild(child_of_url)
+            for link in items.get('icon_links', []):
+                child_of_child_of_url = root.createElement('icon_link')
+                child_of_child_of_url.appendChild(root.createTextNode(link))
+                child_of_url.appendChild(child_of_child_of_url)
 
-            xml.appendChild(urlChild)
+            xml.appendChild(url_child)
 
         xml_str = root.toprettyxml('\t')
         with open(file_address, "w") as f:
